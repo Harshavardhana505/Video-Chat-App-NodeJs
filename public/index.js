c0b6f3a62dab99e78390de8316f581eab5096df4
Harshavardhana <75427218+Harshavardhana505@users.noreply.github.com>
2021-04-19 16:22:15 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ myvideo.muted = true;
 const peerConnections = {}
 navigator.mediaDevices.getUserMedia({
   video:true,
-  audio:false
+  audio:true
 }).then((stream)=>{
   myVideoStream = stream;
   addVideo(myvideo , stream);
